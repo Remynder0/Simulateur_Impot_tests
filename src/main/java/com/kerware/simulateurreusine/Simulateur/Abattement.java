c1135ae063c36af6162532e0571b4c8aafa36d83
Avlis <leo.silva@etu.unicaen.fr>
2026-04-27 17:44:11 +0200

    [ADD] Refonte de l'abattement

--- src/main/java/com/kerware/simulateurreusine/Simulateur/Abattement.java
+++ src/main/java/com/kerware/simulateurreusine/Simulateur/Abattement.java
@@ -1,4 +1,27 @@
 package com.kerware.simulateurreusine.Simulateur;
 
 public class Abattement {
+
+    private final ParametreCalculImpot parametreCalculImpot;
+
+    public Abattement(ParametreCalculImpot parametreCalculImpot) {
+        this.parametreCalculImpot = parametreCalculImpot;
+    }
+
+    private double calculAbattement() {
+        double abt = parametreCalculImpot.getrNet() * ParametreCalculImpotCommun.gettAbt();
+
+        if (abt > ParametreCalculImpotCommun.getlAbtMax()) {
+            abt = ParametreCalculImpotCommun.getlAbtMax();
+        } else if (abt < ParametreCalculImpotCommun.getlAbtMin()) {
+            abt = ParametreCalculImpotCommun.getlAbtMin();
+        }
+
+        return abt;
+    }
+
+     public double calculAbattementNet() {
+         parametreCalculImpot.setAbt(calculAbattement());
+         return parametreCalculImpot.getrNet() - parametreCalculImpot.getAbt();
+     }
 }
